bcbc19546bb1e98ad94fcf1fd41f4b3796cff720
Alex Frankhouser <freesoul423@gmail.com>
2022-12-08 16:06:12 -0500

    Added showRoles() + showDepartment(). Also added .env and config file

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+-- Creates employee_db
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+-- Department Table
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Roles Table
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_ID INT
+);
+
+-- Employees Table
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
--- server.js
+++ server.js
@@ -14,4 +14,136 @@ const connection = mysql.createConnection(
 );
 // ---------------------------------------
 
+connection.connect(function (err) {
+    if (err) return console.log(err);
+    inquirePrompt();
+});
+
+// Inquire Prompt------------------------------------
+const inquirePrompt = () => {
+    inquire.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'Add employee',
+                'Update employee information',
+                'View all roles',
+                'Add role',
+                'View all departments',
+                'Add department',
+                'Quit'
+            ]
+        }
+])
+// ---------------------------------------------------
+    .then((answers) => {
+        const {choices} = answers;
+
+        if (choices === 'View all employees') {
+            showEmployees();
+        };
+
+        if (choices === 'Add employee') {
+            addEmployees();
+        };
+
+        if (choices === 'Update employee information') {
+            employeeInformation();
+        };
+
+        if (choices === 'View all roles') {
+            showRoles();
+        };
+
+        if (choices === 'Add role') {
+            addRoles();
+        };
+
+        if (choices === 'View all departments') {
+            showDepartments();
+        };
+
+        if (choices === 'Add department') {
+            addDepartment();
+        };
+
+        if (choices === 'Quit') {
+            connection.end();
+        };
+    });
+};
+// -------------------------------------------------------
+
+// Show Departments
+showDepartments = () => {
+    console.log("Showing all departments.");
+    const mysql = `SELECT department.id AS ID, department.name AS Department FROM department`;
+
+    connection.query(mysql, (err, rows) => {
+        if(err) return console.log(err);
+        console.table(rows);
+        inquirePrompt();
+    })
+};
+
+// Show Roles
+showRoles = () => {
+    console.log('Displaying all roles');
+    const mysql = `Select roles.id, roles.title, department.name AS Department FROM roles LEFT JOIN department ON roles.department_id = department.id`;
+
+    connection.query(mysql,(err,rows) => {
+        console.table(rows);
+        inquirePrompt();
+    })
+};
+
+// Add Roles
+addRoles = () => {
+    inquire.prompt([
+        {
+            type: 'input',
+            name: 'roles',
+            message: 'What role would you like to add?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the yearly salary?',
+        }
+    ])
+
+    .then(answer => {
+        const parameters = [answer.roles, answer.salary];
+        const roles_var =  `SELECT name, id FROM department`;
+
+        connection.query(roles_var, (err, data) => {
+            if (err) return console.log(err);
+            const department_var = data.map (({ name, id }) => ({name: name, value: id}));
+
+            inquire.prompt([
+                {
+                    type: 'list',
+                    name: 'department_var',
+                    message: 'What department is this role for?',
+                    choices: department_var
+                }
+            ])
+
+            .then(department_varChoice => {
+                const department_var = department_varChoice.department_var;
+                parameters.push(department_var);
+                const mysql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+
+                connection.query(mysql, parameters, (err, result) => {
+                    if(err) return console.log(err);
+                    console.log('Added' + answer.roles + 'to roles');
+                    showRoles();
+                });
+            });
+        });
+    });
+};
 
